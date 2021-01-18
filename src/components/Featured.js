ffd69912bdce8fb74e252a4ef49b2023943900e2
Kevin Lopez <kevin_lopez@live.com>
2021-01-17 22:26:01 -0600

    connects featured to backend

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,12 +1,37 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import FeaturedCard from './FeaturedCard.js'
 import balvenieImg from '../assets/doublewood.jpg'
 import laphroaigImg from '../assets/laphroaig10.jpg'
 
 const Featured = () => {
-    return(
+    const [products, setProducts] = useState([{
+        name: '',
+        price: '',
+        img: ''
+    }])
+
+    useEffect(() => {
+        axios({
+            method: 'get',
+            url: 'http://localhost:5000/products',
+            withCredentials: true
+        })
+            .then(res => {
+                setProducts(res.data)
+                console.log('data set')
+            })
+            .catch(err => console.log(err))
+        }, [])
+
+    console.log('products: ', products)
+
+    return (
         <div className="featured">
-            <FeaturedCard name="BALVENIE DOUBLEWOOD" price="$69.99" img={balvenieImg} />
-            <FeaturedCard name="LAPHROAIG 10" price="$49.99" img={laphroaigImg} />
+            {products.slice(0, 2).map(product => {
+                return <FeaturedCard name={product.name} price={product.price} img={product.img} />
+            })}
         </div>
     )
 }
