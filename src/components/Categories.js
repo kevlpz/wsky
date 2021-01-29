1e9a747e51875d1ad5baa709f4687336ff8b3dc3
Kevin Lopez <kevin_lopez@live.com>
2021-01-28 21:48:17 -0600

    links category cards to pages

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,14 +3,23 @@ import jamesonb from '../assets/jamesonb.jpg'
 import crownroyalb from '../assets/crownroyalb.jpg'
 import johnnywalkerb from '../assets/johnnywalkerb.jpg'
 import jimbeamb from '../assets/jimbeamb.jpg'
+import { Link } from 'react-router-dom'
 
 const Categories = () => {
     return (
         <div className="categories-container">
-            <CategoryCard name="Scotch Whiskey" img={johnnywalkerb} alt="johnny walker bottle" />
-            <CategoryCard name="Bourbon" img={jimbeamb} alt="jim beam bottle" />
-            <CategoryCard name="Irish Whiskey" img={jamesonb} alt="jameson bottle" />
-            <CategoryCard name="Canadian Whiskey" img={crownroyalb} alt="crown royal bottle" />
+            <Link to="/products/1" className="link">
+                <CategoryCard name="Scotch Whiskey" img={johnnywalkerb} alt="johnny walker bottle" />
+            </Link>
+            <Link to="/products/2" className="link">
+                <CategoryCard name="Bourbon" img={jimbeamb} alt="jim beam bottle" />
+            </Link>
+            <Link to="/products/3" className="link">
+                <CategoryCard name="Irish Whiskey" img={jamesonb} alt="jameson bottle" />
+            </Link>
+            <Link to="/products/4" className="link">
+                <CategoryCard name="Canadian Whiskey" img={crownroyalb} alt="crown royal bottle" />
+            </Link>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import Featured from './Featured.js'
 import Banner from './Banner.js'
 import Categories from './Categories.js'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-    return(
+    return (
         <>
             <Featured />
-            <Banner />
+            <Link to="/products/1">
+                <Banner />
+            </Link>
             <Categories />
         </>
     )
