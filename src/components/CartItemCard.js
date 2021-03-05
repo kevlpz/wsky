b1b538118382d1386120fdca74c5e9bfca626806
Kevin Lopez <kevin_lopez@live.com>
2021-03-05 17:18:44 -0600

    .

--- src/components/CartItemCard.js
+++ src/components/CartItemCard.js
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState } from 'react'
 
 const CartItemCard = ({ name, price, quantity, img, productID, removeFromCart, handleQuantityChange }) => {
     const [quantityState, setQuantityState] = useState(quantity)
